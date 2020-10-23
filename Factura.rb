9c05cbf2ee3da76765f6262046f0d20441023ab7
Dennys <dennys1@hotmail.com>
2020-10-23 00:11:57 -0500

    Se agrego al Constructor el hash de descuentos e impuestos

--- Factura.rb
+++ Factura.rb
@@ -1,12 +1,16 @@
 class Factura
     def initialize(cantidad, precioUnitario, estado)
+        @impuesto = {'ut': 6.85, 'nv': 8.0, 'tx': 6.25, 'al':4.0, 'ca':8.25}
+        @descuento = {1000 => 0.03, 5000 => 0.05, 7000 => 0.07, 10000 => 0.10, 50000 => 0.15}
         @cantidad = cantidad
         @precioUnitario = precioUnitario
         @estado = estado
        
     end
     def imprimir
-        "Datos para Facturar:\nCantidad = #{@cantidad}\nPrecio Unitario = #{@precioUnitario}\nEstado = #{@estado}"
+        "Datos para Facturar:\nCantidad = #{@cantidad}\nPrecio Unitario = #{@precioUnitario}\nEstado = #{@estado}
+        \nimpuestos= #{@impuesto}\ndescuento=#{@descuento}"
+
     end
 
 end
