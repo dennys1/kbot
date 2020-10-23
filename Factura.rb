8458e5c2b7408daf1c19eca0e2328601260f5114
Dennys <dennys1@hotmail.com>
2020-10-22 23:46:28 -0500

    Deficion de Factura que recibe los datos y los imprime

--- Factura.rb
+++ Factura.rb
@@ -1,1 +1,14 @@
-puts "Hola Mundo"
+class Factura
+    def initialize(cantidad, precioUnitario, estado)
+        @cantidad = cantidad
+        @precioUnitario = precioUnitario
+        @estado = estado
+       
+    end
+    def imprimir
+        "Datos para Facturar:\nCantidad = #{@cantidad}\nPrecio Unitario = #{@precioUnitario}\nEstado = #{@estado}"
+    end
+
+end
+factura = Factura.new(ARGV[0], ARGV[1], ARGV[2]);
+puts factura.imprimir
